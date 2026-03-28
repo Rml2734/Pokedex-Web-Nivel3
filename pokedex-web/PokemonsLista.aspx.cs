0b7fb82f69ecb655fbc4f32116e0d712bb542686
Robml2734 <Robml2734@gmail.com>
2026-03-28 09:24:48 -0600

    Corregido evento OnClick y limpieza de labels en Formulario

--- pokedex-web/PokemonsLista.aspx.cs
+++ pokedex-web/PokemonsLista.aspx.cs
@@ -15,6 +15,18 @@ namespace pokedex_web
             PokemonNegocio negocio = new PokemonNegocio();
             dgvPokemons.DataSource = negocio.listarConSP();
             dgvPokemons.DataBind();
-        }   
+        }
+
+        protected void dgvPokemons_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            dgvPokemons.PageIndex = e.NewPageIndex;
+            dgvPokemons.DataBind();
+        }
+
+        protected void dgvPokemons_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string id = dgvPokemons.SelectedDataKey.Value.ToString();
+            Response.Redirect("FormularioPokemon.aspx?id=" + id);
+        }
     }
 }
